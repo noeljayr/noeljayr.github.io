ba18e90c85d3e5f7971ae81a1c35caf56c955a2d
Noel Luhanga <108274131+noeljayr@users.noreply.github.com>
2023-08-11 15:33:14 +0200

    Delete ecrowd2 directory

--- ecrowd2/sign-out.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-    include "classes/users.class.php";
-
-        $id = $_GET['id'];
-        $user = new Users();
-        $user->logOut( $id );
-
-        header("location: index.php");
