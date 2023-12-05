b03497a8127cac8274c7496d8b886a9eb9f81e59
Noel Luhanga <108274131+noeljayr@users.noreply.github.com>
2023-12-05 23:03:11 +0200

    Update navbar.js

--- onpoint/js/components/navbar.js
+++ onpoint/js/components/navbar.js
@@ -3,15 +3,15 @@ const navbar = document.querySelector(".navbar");
 if (navbar) {
   navbar.innerHTML = `
 
-     <a href=".../index.html" class="logo">
+     <a href="./" class="logo">
         <img src="public/logos/logo.png" alt="" />
         <span class="full">On-point Finance and Business Consultants</span>
         <span class="abriv">OPFBC</span>
       </a>
       <span class="links">
-        <a class="navlink" href=".../index.html">Home</a>
+        <a class="navlink" href="./">Home</a>
 
-        <a class="navlink" href="index.html#services">Services</a>
+        <a class="navlink" href="./index.html#services">Services</a>
         <a class="navlink" href="media.html">Media</a>
         <a class="navlink" href="contact.html">Contact us</a>
         <a class="navlink" href="profile.html">Profile</a>
@@ -33,6 +33,7 @@ if (navbar) {
           <a class="" href="media.html">Media</a>
           <a class="" href="contact.html">Contact us</a>
           <a class="" href="profile.html">Profile</a>
+          <a class="cta" href="register.html">Work with us</a>
         </span>
       </span>
 
