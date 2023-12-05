e76255b6c22df63260d2d361ea2e514de98190fe
Noel Luhanga <108274131+noeljayr@users.noreply.github.com>
2023-12-05 21:53:52 +0200

    Update select-input.js

--- onpoint/js/select-input.js
+++ onpoint/js/select-input.js
@@ -36,6 +36,44 @@ options.forEach((o) => {
     o.classList.add("selected-option");
 
     formSelect.value = o.textContent.trim();
+    var event = new Event("change");
+
+    // Dispatch the event on the element
+    formSelect.dispatchEvent(event);
     selectedT.textContent = o.textContent.trim();
+
+    if (page === "reg") {
+       var submitBtn = document.querySelector("form button");
+       var appForm = document.querySelector("form");
+    
+
+       function preventDefaultBehaviour(event) {
+         event.preventDefault();
+         showApplicationMode();
+       }
+
+      
+
+       function handleFormSelectChange() {
+         if (formSelect.value == "Loan") {
+           showApplicationMode();
+           submitBtn.addEventListener("click", showApplicationMode);
+           appForm.addEventListener("submit", preventDefaultBehaviour);
+         } else {
+           submitBtn.removeEventListener("click", showApplicationMode);
+           appForm.removeEventListener("submit", preventDefaultBehaviour);
+         }
+       }
+
+       // Initial check
+       handleFormSelectChange();
+
+       // Event listener for changes to the select element
+       formSelect.addEventListener("change", handleFormSelectChange);
+    }
   });
 });
+
+// formSelect.addEventListener("change", () => {
+//   console.log(formSelect.value);
+// });
